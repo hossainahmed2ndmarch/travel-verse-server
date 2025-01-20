fb7ab2b569737bc8b9d0fe79209c08a25241b18f
hossainahmed2ndmarch <hossainahmed2ndmarch@gmail.co>
2025-01-20 23:29:33 +0600

    Added guides API

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.c6qkm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,6 +27,7 @@ async function run() {
   await client.connect();
   const tourCollection = client.db('travelDB')
   const packages = tourCollection.collection('packages')
+  const guides = tourCollection.collection('guides')
 
   // Packages API
   app.get('/packages-home', async (req, res) => {
@@ -37,6 +38,26 @@ async function run() {
 
   app.get('/packages', async (req, res) => {
    const cursor = packages.find()
+   const result = await cursor.toArray()
+   res.send(result)
+  })
+
+  app.get('/packages/:id', async (req, res) => {
+   const id = req.params.id
+   const package = { _id: new ObjectId(id) }
+   const result = await packages.findOne(package)
+   res.send(result)
+  })
+
+  // Guides API
+  app.get('/guides', async(req, res)=>{
+   const cursor = guides.find()
+   const result = await cursor.toArray()
+   res.send(result)
+  })
+
+  app.get('/guides-home', async (req, res) => {
+   const cursor = guides.aggregate([{ $sample: { size: 6 } }])
    const result = await cursor.toArray()
    res.send(result)
   })
