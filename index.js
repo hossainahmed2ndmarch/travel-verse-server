78bb36c9eb1d36dbf16b59dfa413b564030452df
hossainahmed2ndmarch <hossainahmed2ndmarch@gmail.co>
2025-01-25 10:46:18 +0600

    Added makeadmin API

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express()
+const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
 const port = process.env.PORT || 5000
@@ -109,6 +110,17 @@ async function run() {
    res.send(result)
   })
 
+  app.patch('/users/admin/:id', async (req, res) => {
+   const id = req.params.id
+   const filter = { _id: new ObjectId(id) }
+   const updatedDoc = {
+    $set: {
+     role: 'admin'
+    }
+   }
+   const result = await users.updateOne(filter, updatedDoc)
+   res.send(result)
+  })
   app.delete('/users/:id', async (req, res) => {
    const id = req.params.id
    const query = { _id: new ObjectId(id) }
